2989fa723aa837a2d0eeb91632203c000a5f9736
Developer Baboo <babooheerani@gmail.com>
2023-10-23 00:24:02 +0500

    last commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,9 +13,61 @@ class PostController extends Controller
         ]);
     }
     function view($slug){
-        dd($slug);
-        // $post = Post::whereSlug($slug)
-        //     ->first();
-        // return view('details', ['post' => $post]);
+        /*
+        // onloy post info
+        $post = Post::whereSlug($slug)->first();
+        dd($post->toArray()); */
+
+        //post with user, tag info, ka ya post kis na likhe ha or us pa kitna and konsa tags han
+        /* $post = Post::whereSlug($slug)
+            ->with(['user', 'tags'])
+            ->first();
+        dd($post->toArray()); */
+
+        //post with user, tag info and us pr kitna likes han
+        /* $post = Post::whereSlug($slug)
+            ->withCount(['likes'])
+            ->with(['user', 'tags'])
+            ->first();
+        dd($post->toArray()); */
+
+        /* //post with user, tag info and us pr kitna likes han with likes info ka ya like kis user na kya ha
+        $post = Post::whereSlug($slug)
+            ->withCount(['likes'])
+            ->with([
+                'user',
+                'tags',
+                'likes.user'
+                ])
+            ->first();
+            dd($post->toArray());
+            */
+
+            //post with user, tag info and us pr kitna likes han with likes info ka ya like kis user na kya ha with comment
+        $post = Post::whereSlug($slug)
+            ->withCount(['likes'])
+            ->with([
+                'user',
+                'tags',
+                //we have defined the user relation in like model
+                'likes.user',
+                //we have defined user relation in comment model
+                'comments.user',
+                //comments like
+                'comments.likes',
+                //comments replies with user info ka kis na commnet pa reply kya. us reply pa kitna like aya han
+
+                'comments.replies'=>function($q){
+                    $q->with('user');
+                    $q->withCount('likes');
+                    $q->with('likes.user');
+
+                }
+                ])
+            ->first();
+            dd($post->toArray());
+
+
+
     }
 }
